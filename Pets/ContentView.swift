3a59d3317e9c2896f91d42268c6a3f71c0ddb84d
Metin Atalay <metin.atalay@trtworld.com>
2022-06-09 18:18:45 +0300

    Initial Commit

---
+++ Pets/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Pets
+//
+//  Created by Metin Atalay on 9.06.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pets/PetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetsApp.swift
+//  Pets
+//
+//  Created by Metin Atalay on 9.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct PetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
